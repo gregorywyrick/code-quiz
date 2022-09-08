be6fccb111cc4c64ceef3eacae11c8d870cbc2af
gregorywyrick <gregmwyrick@gmail.com>
2022-09-08 13:25:32 -0500

    variables defined

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var timeEl = document.querySelector("#time");
+var startBtn = document.querySelector("#startButton");
+var submitBtn = document.querySelector("#submit-button");
+var titleScreen = document.querySelector("#title-section");
+var quizScreen = document.querySelector("#quiz-section");
+var highScoreScreen = document.querySelector("#highscore-section");
+var highScoreDisplay = document.querySelector("#highscore-display-section");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+var questionsEl = document.querySelector("#question");
+var choicesEl = document.querySelector("#choices");
