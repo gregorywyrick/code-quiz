cd59223e7507f5f21e9cc2c641107e6175bcf93f
gregorywyrick <gregmwyrick@gmail.com>
2022-09-08 13:23:56 -0500

    questions added

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "What operator is used to represent OR:",
+        choices: ["=+", "===", "||", "&&"],
+        answer: "||"
+    },
+    {
+        title: "When a condition in an IF statment is not met, what will the code default to",
+        choices: ["else", "else if", "if if", "return"],
+        answer: "else"
+    },
+    {
+        title: "What JS method can be used to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementByName"],
+        answer: "document.queryselector()"
+    },
+    {
+        title: "What operator is used to see if two things are exactly equal?",
+        choices: ["..", "=+", "===", "=&="],
+        answer: "==="
+    },
+    {
+        title: "T or F: Functions can be passed as an arguemtn to other functions?",
+        choices: ["True", "False"],
+        answer: "True"
+    }
+]
